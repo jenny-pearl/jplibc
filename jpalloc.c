30961413be4f760dfc046c26b199303afafe144a
Jenny <aalper0138@gmail.com>
2025-12-30 16:06:19 +0300

    Add files via upload

--- jpalloc.c
+++ jpalloc.c
@@ -5,9 +5,6 @@
 #include <stdbool.h>
 #include <sys/mman.h>
 
-#define PAGE_SIZE (getpagesize())
-#define WORD (sizeof(void *))
-
 typedef struct {
         uint64_t pos;
         uint64_t size;
@@ -21,16 +18,10 @@ typedef struct {
 
 mem_arena *jp_malloc(uint64_t size);
 void jp_free(mem_arena *block);
-void *jp_push_to_block(mem_arena *block, uint64_t size);
 
 int main(int argc, char **argv)
 {
         mem_arena *block = jp_malloc(PAGE_SIZE);
-        Foo *foo = jp_push_to_block(block, sizeof(Foo));
-        foo->a = 3;
-        foo->b = 4;
-        foo->c = 3.2;
-        printf("%d %d %f\n", foo->a, foo->b, foo->c);
         jp_free(block);
         return 0;
 }
@@ -48,21 +39,4 @@ mem_arena *jp_malloc(uint64_t len)
 void jp_free(mem_arena *block)
 {
         munmap(block, block->size);
-}
-
-uint64_t arch_align(uint64_t n, uint64_t p)
-{
-        return (((n) + ((p) - 1)) & (~((p) - 1)));
-}
-
-void *jp_push_to_block(mem_arena *block, uint64_t len)
-{
-        uint64_t aligned = arch_align(block->pos, WORD);
-        uint64_t new = aligned + len;
-
-        if (new > block->size) assert(false);
-
-        block->pos = new;
-
-        return ((uint8_t *)block + aligned);
 }
